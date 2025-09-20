3aa8f3c6042d50614a3947c99f684054b5248ecb
vijayalakshmisvijayalakshmi72-commits <vijayalakshmisvijayalakshmi72@gmail.com>
2025-09-20 11:47:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
